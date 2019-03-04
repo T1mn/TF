bc0163e1f66a5c0520445c62a5521996d4b7db91
TimJ3ly <TimJ3ly@gmail.com>
2019-03-04 15:36:48 +0800

    增加 理解loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),reduction_indices=[1]))代码

--- 周莫烦/Tensorflow 11 例子3 建造神经网络 build a neural network (神经网络 教学教程tutorial).py
+++ 周莫烦/Tensorflow 11 例子3 建造神经网络 build a neural network (神经网络 教学教程tutorial).py
@@ -3,7 +3,7 @@ import numpy as np
 
 def add_layer(inputs,in_size,out_size,activation_function = None):
     Weights = tf.Variable(tf.random_normal([in_size,out_size]))
-    biases = tf.zeros([1,out_size]) + 0.1
+    biases = tf.Variable(tf.zeros([1,out_size]) + 0.1)
     Wx_plus_biases = tf.matmul(inputs,Weights) + biases
 
     if activation_function is None:
@@ -12,27 +12,33 @@ def add_layer(inputs,in_size,out_size,activation_function = None):
         outputs = activation_function(Wx_plus_biases)
     return outputs
 
-x_data = np.linspace(-1,1,300)[:,np.newaxis]
-
+x_data = np.linspace(-1,1,50)[:,np.newaxis]
 noise = np.random.normal(0,0.05,x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise
 
-xs = tf.placeholder(tf.float32)
-ys = tf.placeholder(tf.float32)
+xs = tf.placeholder(tf.float32,[None,1])
+ys = tf.placeholder(tf.float32,[None,1])
 
-l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
-prediction = add_layer(l1, 10, 1, activation_function=None)#出现错误：ValueError: None values not supported.
+l1 = add_layer(xs,1,15,activation_function=tf.nn.relu)
+prediction = add_layer(l1,15,1,activation_function=None)
 
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
-                     reduction_indices=[1]))
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
-init = tf.global_variables_initializer()  
+init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
-for i in range(100000):
-    # training
-    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
-    if i % 500 == 0:
-        # to see the step improvement
-        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+for _ in range(2000):
+    sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
+    if _ % 200 == 0:
+        print(sess.run(loss,feed_dict= {xs:x_data,ys:y_data}))
+
+
+# 理解loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),reduction_indices=[1]))所用
+# tf.reduce_sum( ,reduction_indices=[1]) 横着相加，并转置
+
+# print('y_data=',y_data)
+# print('prediction=',sess.run(prediction,feed_dict={xs:x_data,ys:y_data}))
+# print('y_data-prediction=',sess.run(y_data-prediction,feed_dict={xs:x_data,ys:y_data}))
+# print('tf.square(ys - prediction)=',sess.run(tf.square(ys - prediction),feed_dict={xs:x_data,ys:y_data}))
+# print('tf.reduce_sum(tf.square(ys - prediction),reduction_indices=[1])=',sess.run(tf.reduce_sum(tf.square(ys - prediction),reduction_indices=[1]),feed_dict={xs:x_data,ys:y_data}))
